ae66f4357273f1c1af7c13a2bec2c6f6cf329a09
sya-ri <git@s7a.dev>
2022-08-07 12:22:25 +0900

    feat(examples/jvm): Add uploadFile example

--- examples/jvm/build.gradle.kts
+++ examples/jvm/build.gradle.kts
@@ -7,6 +7,12 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("dev.s7a:gofile:1.0.0-SNAPSHOT")
+    implementation("io.ktor:ktor-client-cio:2.0.3")
 }
 
 application {
--- examples/jvm/src/main/kotlin/dev/s7a/example/gofile/Main.kt
+++ examples/jvm/src/main/kotlin/dev/s7a/example/gofile/Main.kt
@@ -2,5 +2,18 @@
 
 package dev.s7a.example.gofile
 
-fun main() {
+import dev.s7a.gofile.GofileClient
+import dev.s7a.gofile.uploadFile
+import kotlin.io.path.createTempFile
+import kotlin.io.path.writeText
+
+suspend fun main() {
+    val client = GofileClient()
+    val file = createTempFile(suffix = ".txt").apply {
+        writeText("Hello world.")
+    }.toFile()
+    client.uploadFile(file).fold(
+        onSuccess = { println("Download page: ${it.downloadPage}") },
+        onFailure = Throwable::printStackTrace
+    )
 }
