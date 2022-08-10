7a577f643986c4a47804bdf8e6da83aab2516c16
sya-ri <git@s7a.dev>
2022-08-10 13:37:36 +0900

    feat(example/e2e): Add more methods

--- examples/e2e/src/main/kotlin/dev/s7a/example/gofile/Main.kt
+++ examples/e2e/src/main/kotlin/dev/s7a/example/gofile/Main.kt
@@ -3,6 +3,7 @@
 package dev.s7a.example.gofile
 
 import dev.s7a.gofile.GofileClient
+import dev.s7a.gofile.GofileFolderOption
 import dev.s7a.gofile.uploadFile
 import kotlin.io.path.createTempFile
 import kotlin.io.path.writeText
@@ -13,8 +14,14 @@ suspend fun main() {
     val file = createTempFile(suffix = ".txt").apply {
         writeText("Hello world.")
     }.toFile()
-    client.uploadFile(file, token = token).fold(
-        onSuccess = { println("Download page: ${it.downloadPage}") },
-        onFailure = Throwable::printStackTrace
-    )
+    val uploadFile = client.uploadFile(file, token = token).getOrThrow()
+    println("Download page: ${uploadFile.downloadPage}")
+    if (token == null) return
+    val createFolder = client.createFolder(uploadFile.parentFolder, "new-folder", token).getOrThrow()
+    client.setFolderOption(createFolder.id, GofileFolderOption.Description("description"), token).getOrThrow()
+    client.setFolderOption(createFolder.id, GofileFolderOption.Expire(1660106197), token).getOrThrow()
+    client.setFolderOption(createFolder.id, GofileFolderOption.Password("password"), token).getOrThrow()
+    client.setFolderOption(createFolder.id, GofileFolderOption.Tags("t", "a", "g", "s"), token).getOrThrow()
+    client.copyContent(uploadFile.fileId, createFolder.id, token).getOrThrow()
+    client.deleteContent(uploadFile.fileId, token).getOrThrow()
 }
