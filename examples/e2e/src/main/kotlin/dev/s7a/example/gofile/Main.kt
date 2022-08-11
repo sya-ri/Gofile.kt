8efc6da45bcf25ee955ef0f8c306244b26ece621
sya-ri <git@s7a.dev>
2022-08-11 15:23:39 +0900

    feat(example/e2e): Use guestToken

--- examples/e2e/src/main/kotlin/dev/s7a/example/gofile/Main.kt
+++ examples/e2e/src/main/kotlin/dev/s7a/example/gofile/Main.kt
@@ -10,18 +10,19 @@ import kotlin.io.path.writeText
 
 suspend fun main() {
     val client = GofileClient()
-    val token: String? = System.getenv("GOFILE_TOKEN")
+    var token: String? = System.getenv("GOFILE_TOKEN")
     val file = createTempFile(suffix = ".txt").apply {
         writeText("Hello world.")
     }.toFile()
     val uploadFile = client.uploadFile(file, token = token).getOrThrow()
     println("Download page: ${uploadFile.downloadPage}")
-    if (token == null) return
+    if (token == null) token = uploadFile.guestToken ?: return
     val createFolder = client.createFolder(uploadFile.parentFolder, "new-folder", token).getOrThrow()
     client.setFolderOption(createFolder.id, GofileFolderOption.Description("description"), token).getOrThrow()
     client.setFolderOption(createFolder.id, GofileFolderOption.Expire(1660106197), token).getOrThrow()
     client.setFolderOption(createFolder.id, GofileFolderOption.Password("password"), token).getOrThrow()
     client.setFolderOption(createFolder.id, GofileFolderOption.Tags("t", "a", "g", "s"), token).getOrThrow()
-    client.copyContent(uploadFile.fileId, createFolder.id, token).getOrThrow()
+    // Only premium user
+    // client.copyContent(uploadFile.fileId, createFolder.id, token).getOrThrow()
     client.deleteContent(uploadFile.fileId, token).getOrThrow()
 }
