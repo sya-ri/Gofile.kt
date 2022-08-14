e9644c490a4a50a8196364be6c1574c9cf8fe547
sya-ri <git@s7a.dev>
2022-08-14 17:02:36 +0900

    feat(examples/jvm): Add upload process

--- examples/jvm/src/main/kotlin/dev/s7a/example/gofile/Main.kt
+++ examples/jvm/src/main/kotlin/dev/s7a/example/gofile/Main.kt
@@ -2,5 +2,46 @@
 
 package dev.s7a.example.gofile
 
-fun main() {
+import dev.s7a.gofile.GofileClient
+import dev.s7a.gofile.uploadFile
+import java.io.File
+import kotlin.system.exitProcess
+
+suspend fun main() {
+    val token: String? = System.getenv("GOFILE_TOKEN")
+    val client = GofileClient()
+    val file = inputFile()
+    client.uploadFile(file, token = token).fold(
+        onSuccess = {
+            println("Download page: ${it.downloadPage}")
+        },
+        onFailure = {
+            it.printStackTrace()
+            exitProcess(ExitCode.UploadFailed)
+        }
+    )
+}
+
+private object ExitCode {
+    const val NoInputPath = 1
+    const val NoSuchFile = 2
+    const val IsNotFile = 3
+    const val UploadFailed = 4
+}
+
+private fun inputFile(): File {
+    print("File path to upload: ")
+    val filePath = readLine() ?: exitProcess(ExitCode.NoInputPath)
+    return File(filePath).apply {
+        when {
+            exists().not() -> {
+                println("$filePath: No such file.")
+                exitProcess(ExitCode.NoSuchFile)
+            }
+            isFile.not() -> {
+                println("$filePath: Is not file.")
+                exitProcess(ExitCode.IsNotFile)
+            }
+        }
+    }
 }
