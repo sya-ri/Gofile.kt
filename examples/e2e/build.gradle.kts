bdf3bbf408889554e52f10c8c142de8aae0fcf06
sya-ri <git@s7a.dev>
2022-09-07 23:45:01 +0900

    chore: Bump dependencies - ktor: 2.1.0 -> 2.1.1

--- examples/e2e/build.gradle.kts
+++ examples/e2e/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation("dev.s7a:gofile:1.0.0")
-    implementation("io.ktor:ktor-client-cio:2.1.0")
+    implementation("io.ktor:ktor-client-cio:2.1.1")
 }
 
 application {
--- examples/native/build.gradle.kts
+++ examples/native/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
         val nativeMain by getting {
             dependencies {
                 implementation("dev.s7a:gofile:1.0.0")
-                implementation("io.ktor:ktor-client-cio:2.1.0")
+                implementation("io.ktor:ktor-client-cio:2.1.1")
                 implementation("com.squareup.okio:okio:3.2.0")
             }
         }
