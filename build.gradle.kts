4add4cc5b0d9e7987fe0e554b2001388ec00f42b
sya-ri <git@s7a.dev>
2023-11-11 21:13:19 +0900

    chore: Fix publish task name

--- build.gradle.kts
+++ build.gradle.kts
@@ -182,7 +182,7 @@ publishing {
 
 afterEvaluate {
     val targetNames = kotlin.targets.names.map(String::capitalized)
-    val publishTasks = targetNames.mapNotNull { tasks.findByName("publish${it}ToMavenRepository") }
+    val publishTasks = targetNames.mapNotNull { tasks.findByName("publish${it}PublicationToMavenRepository") }
     val signTasks = targetNames.mapNotNull { tasks.findByName("sign${it}Publication") }
 
     publishTasks.forEach { publishTask ->
